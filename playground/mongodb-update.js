55b695f66fcd357ae1d166fc94f5412a42dfa1c8
Slobodan Stojcic <slobodan@nrds.be>
2019-02-14 14:39:20 +0100

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,39 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err){
+        return console.log('Unable to connect to MongoDB server');
+    }    
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID('5c65658a8a49443be4a0d026')
+    // }, {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID('5c654b63c5bc4a43b8816fa2')
+    }, {
+        $set: {
+            name: 'Slobodan'
+        },
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    });
+
+    //client.close();
+});
